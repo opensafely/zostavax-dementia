8e8037ec4a7b0fc3f9e174c39c5caf057a680c0e
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2026-08-17 09:20:04 +0000

    Return df in R

--- analysis/zostavax/0_zostavax_processing.R
+++ analysis/zostavax/0_zostavax_processing.R
@@ -5,10 +5,10 @@ library(arrow)
 source(here::here("analysis", "common_code", "processing.R"))
 
 df_main <- read_feather(here::here("output","zostavax","dataset_zostavax_main.arrow"))
-processing(df_main, as.Date("2013-09-01"), as.Date("2014-02-01"), "zostavax","main")
+df_processed_main <- processing(df_main, as.Date("2013-09-01"), as.Date("2014-02-01"), "zostavax","main")
 
 df_2010 <- read_feather(here::here("output","zostavax","dataset_zostavax_2010.arrow"))
-processing(df_2010, as.Date("2010-09-01"), as.Date("2011-02-01"), "zostavax","2010")
+df_processed_2010 <- processing(df_2010, as.Date("2010-09-01"), as.Date("2011-02-01"), "zostavax","2010")
 
 df_2016 <- read_feather(here::here("output","zostavax","dataset_zostavax_2016.arrow"))
-processing(df_2016, as.Date("2016-09-01"), as.Date("2017-02-01"), "zostavax","2016")
+df_processed_2016 <- processing(df_2016, as.Date("2016-09-01"), as.Date("2017-02-01"), "zostavax","2016")
